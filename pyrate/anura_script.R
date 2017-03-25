00cbcf6cdc6ec607dd1564f4242ed117710012ef
Marina Salama <mfsalama@g.ucla.edu>
2017-03-24 21:53:48 -0700

    pyrate graphs for final paper

---
+++ pyrate/anura_script.R
@@ -0,0 +1,21 @@
+```{r}
+source("~/PyRate/pyrate_utilities.r")
+
+# we need to give the utilities a list of extant species
+extant_frogs = c("Bufo viridis","Bufo woodhousei","Hyla barbudensis","Scaphiopus couchi","Spea hammondii","Rana dalmatina")
+
+# use the extract.ages.pbdb() function in pyrate_utilities to reformat our dataset...
+extract.ages.pbdb(file= "anura_occ.csv",extant_species=extant_frogs)
+
+# you should see the following output:
+
+# "This function is currently being tested - caution with the results!"
+# replicate 1
+# PyRate input file was saved in:  ./canid_occ_PyRate.py 
+
+
+# TO DO:
+# save the file as "process_canid_data.R" in exercise-9
+
+# quit rstudio
+```{r}
